3a2f2cbede32e41c57e61d29206f28bb1eae3227
chrwinkler <101516854+chrwinkler@users.noreply.github.com>
2022-11-28 10:48:06 +0100

    still working on finishing assignment

--- src/fib.c
+++ src/fib.c
@@ -3,11 +3,24 @@
  */
 #include "fib.h"
 
-#include <assert.h>		/* assert */
-#include <stdio.h>		/* printf */
+#include <assert.h> /* assert */
+#include <stdio.h>  /* printf */
 
 /* Fibonacci function definition */
-int fib (int n, int p, int pp)
+int fib(int n, int p, int pp)
 {
+    {
+        /* pre-condition */
+        assert(n >= 1);
+        /* post-condition */
+        if (n == 1)
+            return 1;
+        else if (n == 2)
+            return 1;
+        else
+
+            return fib(n - 1, p, pp) + fib(n - 2, );
+    }
+
     return 0;
 }
